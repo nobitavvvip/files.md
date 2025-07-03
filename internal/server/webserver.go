1b666a8ca9b83046ca4dbf7b97e49d00de583d75
Artem Zakirullin <artemzr@gmail.com>
2025-07-03 15:21:00 +0300

    add cors for version

--- internal/server/webserver.go
+++ internal/server/webserver.go
@@ -210,7 +210,7 @@ func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-OneTimeToken")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, X-CSRF-OneTimeToken, Version")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
