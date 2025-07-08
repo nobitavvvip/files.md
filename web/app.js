c13ec631b5a951cc82f9a7b59a59014ef9a1928a
Artem Zakirullin <artemzr@gmail.com>
2025-07-08 11:32:03 +0300

    add low priority entries

--- web/app.js
+++ web/app.js
@@ -475,7 +475,7 @@ function createAutocompleteDict() {
     });
 
     let lowPriorityEntries = [];
-    Object.keys('_read_', '_watch_', '_shop_', 'today', 'later').forEach(dir => {
+    Object.keys(['_read_', '_watch_', '_shop_', 'today', 'later']).forEach(dir => {
         Object.keys(files[dir]).forEach(filename => {
             if (filename === CONFIG_FILENAME || filename === CHAT_FILENAME) {
                 return;
@@ -496,7 +496,7 @@ function createAutocompleteDict() {
     lowPriorityEntries.forEach(entry => {
         dict[entry.key] = entry.filePath;
     });
-    
+
     return dict;
 }
 
