d41b94b1b7ab9feee49efb73a3782319a1d76adf
Artem Zakirullin <artemzr@gmail.com>
2025-10-16 11:03:55 +0300

    rem wails desktop implementation

--- cmd/chat/frontend/src/bot.js
+++
@@ -1,9 +0,0 @@
-import './bot.css';
-
-import {Send} from '../wailsjs/go/main/App';
-import {NewUpdate} from '../wailsjs/go/main/App';
-import {NewCmd} from '../wailsjs/go/main/App';
-
-window.send = Send
-window.newUpdate = NewUpdate
-window.newCmd = NewCmd
